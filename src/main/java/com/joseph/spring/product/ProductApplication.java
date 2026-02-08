23445770e09ae689963b446cf154ba338051994d
José <walterandrade0001@gmail.com>
2026-02-08 14:12:10 -0300

    Exclusion: DB_USERNAME variable

--- src/main/java/com/joseph/spring/product/ProductApplication.java
+++ src/main/java/com/joseph/spring/product/ProductApplication.java
@@ -10,7 +10,6 @@ public class ProductApplication {
     static void main(String[] args) {
 
         Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
         SpringApplication.run(ProductApplication.class, args);
