798034f5dd3c05eb23b0e7c73ba8b0e3e10d9ab1
José <walterandrade0001@gmail.com>
2026-02-15 22:20:34 -0300

    Fix: Exclusion of unused imports and CommandLineRunner

--- src/main/java/com/joseph/spring/product/ProductApplication.java
+++ src/main/java/com/joseph/spring/product/ProductApplication.java
@@ -1,11 +1,8 @@
 package com.joseph.spring.product;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class ProductApplication {
@@ -16,10 +13,5 @@ public class ProductApplication {
         System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
         SpringApplication.run(ProductApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
-        return args -> kafkaTemplate.send("product", "Starting...");
     }
 }
